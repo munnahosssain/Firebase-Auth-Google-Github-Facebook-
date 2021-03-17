f773f5b6e2369f2d8bd7efc3d35ce5db0e9462e9
Munna Hossain <mayinuddin.munna@gmail.com>
2021-03-17 16:10:10 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+import { useState } from 'react';
+import { Button } from '@material-ui/core';
 
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [user, setUser] = useState({});
+    const googleProvider = new firebase.auth.GoogleAuthProvider();
+    const fbProvider = new firebase.auth.FacebookAuthProvider();
+    const githubProvider = new firebase.auth.GithubAuthProvider();
+
+    const handleGoogleSignIn = () => {
+        firebase.auth()
+            .signInWithPopup(googleProvider)
+            .then((result) => {
+                var credential = result.credential;
+                var token = credential.accessToken;
+                var user = result.user;
+                console.log("Google User", user, 'Token', token);
+                setUser(user);
+            }).catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                var email = error.email;
+                var credential = error.credential;
+                console.log(errorCode, errorMessage, email, credential);
+
+            });
+    }
+    const handleFbSignIn = () => {
+        firebase.auth()
+            .signInWithPopup(fbProvider)
+            .then((result) => {
+                var credential = result.credential;
+                var user = result.user;
+                var accessToken = credential.accessToken;
+                console.log(accessToken);
+                console.log("Facebook user", user);
+                setUser(user);
+            })
+            .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                var email = error.email;
+                var credential = error.credential;
+                console.log(errorCode, errorMessage, email, credential);
+            });
+    }
+    const handleGitHubSignIn = () => {
+        firebase.auth()
+            .signInWithPopup(githubProvider)
+            .then((result) => {
+                var credential = result.credential;
+                var token = credential.accessToken;
+                var user = result.user;
+                console.log("Github User: ", user, "Token: ", token);
+                setUser(user);
+            }).catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                var email = error.email;
+                var credential = error.credential;
+                console.log(errorCode, errorMessage, email, credential);
+            });
+    }
+    return (
+        <div className="App-header">
+            <Button variant="contained" color="primary" onClick={handleGoogleSignIn}>google</Button><br />
+            <Button variant="contained" color="primary" onClick={handleFbSignIn}>Facebook</Button> <br />
+            <Button variant="contained" color="primary" onClick={handleGitHubSignIn}>GitHub</Button>
+
+            <h3>User: {user.displayName}</h3>
+            <img src={user.photoURL} alt="" />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAT38LmZ72f2JBba7DQiFfUDUnJNYrudgg",
+    authDomain: "rirebase-auth-recap.firebaseapp.com",
+    projectId: "rirebase-auth-recap",
+    storageBucket: "rirebase-auth-recap.appspot.com",
+    messagingSenderId: "835573311767",
+    appId: "1:835573311767:web:b8bf160e848a7fda51ef52"
+  };
+  export default firebaseConfig;
